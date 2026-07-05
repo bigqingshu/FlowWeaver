c03e9a5600f75b52a89f8a5ef364ee6c88b23290
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-05 20:09:01 +0800

    修复通知组件加载并整理文档目录

--- Avalonia_UI/Views/Components/Shell/ShellNotificationHostView.axaml.cs
+++ Avalonia_UI/Views/Components/Shell/ShellNotificationHostView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
 namespace Avalonia_UI.Views.Components.Shell;
 
@@ -6,6 +7,6 @@ public partial class ShellNotificationHostView : UserControl
 {
     public ShellNotificationHostView()
     {
-        InitializeComponent();
+        AvaloniaXamlLoader.Load(this);
     }
 }
