b8f0fcf29f63be7d311536d67bad6df3d79ff0c8
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 08:05:43 +0800

    拆分工作流定义展示辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Document.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Document.cs
@@ -5,62 +5,6 @@ namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    public bool HasWorkflowDefinition => WorkflowDefinitionDetail is not null;
-
-    public bool HasWorkflowDefinitionError =>
-        !string.IsNullOrWhiteSpace(WorkflowDefinitionErrorMessage);
-
-    public bool IsWorkflowDefinitionDraftBusy =>
-        IsValidatingWorkflowDefinitionDraft || IsSavingWorkflowDefinitionDraft;
-
-    public bool HasWorkflowDefinitionValidationError =>
-        !string.IsNullOrWhiteSpace(WorkflowDefinitionValidationErrorMessage);
-
-    public bool HasWorkflowDefinitionDraft => !string.IsNullOrWhiteSpace(WorkflowDefinitionDraftJson);
-
-    public string WorkflowRunGuardText
-    {
-        get
-        {
-            if (HasWorkflowDefinitionRevisionConflict)
-            {
-                return T("workflow.run_guard_revision_conflict");
-            }
-
-            return IsWorkflowDefinitionDraftDirty
-                ? T("workflow.run_guard_dirty_draft")
-                : T("workflow.run_guard_saved_revision");
-        }
-    }
-
-    public string WorkflowDefinitionSectionText => T("definition.section");
-
-    public string DetailsText => T("definition.details");
-
-    public string NameLabelText => T("definition.name");
-
-    public string VersionLabelText => T("definition.version");
-
-    public string RevisionLabelText => T("definition.revision");
-
-    public string StatusLabelText => T("definition.status");
-
-    public string HashLabelText => T("definition.hash");
-
-    public string UpdatedLabelText => T("definition.updated");
-
-    public string DraftJsonSectionText => T("definition.draft_json");
-
-    public string ShowAdvancedDraftJsonText => IsWorkflowDraftJsonAdvancedVisible
-        ? T("definition.hide_draft_json")
-        : T("definition.show_draft_json");
-
-    public string ValidateText => T("definition.validate");
-
-    public string RestoreText => T("definition.restore");
-
-    public string SaveText => T("definition.save");
-
     partial void OnWorkflowDefinitionDetailChanged(WorkflowDefinitionDetailViewModel? value)
     {
         ClearWorkflowDefinitionDraftBatchSelection();
