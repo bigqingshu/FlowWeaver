ec9eddbcaa92c51bac5bde9fb7af3dfa8d7c8026
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 16:02:21 +0800

    拆分共享发布版本刷新辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.SharedPublications.Versions.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.SharedPublications.Versions.cs
@@ -1,8 +1,6 @@
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using Avalonia_UI.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 namespace Avalonia_UI.ViewModels;
 
@@ -31,85 +29,6 @@ public partial class MainWindowViewModel
 
     public bool HasSharedPublicationVersionError =>
         !string.IsNullOrWhiteSpace(SharedPublicationVersionErrorMessage);
-
-    private bool CanRefreshSharedPublicationVersions()
-    {
-        return CanUseEngineActions
-            && !IsLoadingSharedPublicationVersions
-            && (NormalizeFilter(SharedPublicationVersionShareNameFilter) is not null
-                || SelectedSharedPublication is not null);
-    }
-
-    [RelayCommand(CanExecute = nameof(CanRefreshSharedPublicationVersions))]
-    private async Task RefreshSharedPublicationVersionsAsync()
-    {
-        var shareName = NormalizeFilter(SharedPublicationVersionShareNameFilter)
-            ?? SelectedSharedPublication?.ShareName;
-        if (string.IsNullOrWhiteSpace(shareName))
-        {
-            SharedPublicationVersionMessage = T("data.shared_publication_versions_rejected");
-            SharedPublicationVersionErrorMessage =
-                T("data.share_name_required_for_versions");
-            return;
-        }
-
-        if (!TryParseLimit(
-            SharedPublicationVersionLimitFilter,
-            T("data.shared_publication_version_limit_label"),
-            out var limit,
-            out var error))
-        {
-            SharedPublicationVersionMessage = T("data.shared_publication_versions_rejected");
-            SharedPublicationVersionErrorMessage = error;
-            return;
-        }
-
-        var requestVersion = ++sharedPublicationVersionsLoadVersion;
-        IsLoadingSharedPublicationVersions = true;
-        SharedPublicationVersionMessage = F("format.loading_versions_for", shareName);
-        SharedPublicationVersionErrorMessage = null;
-
-        try
-        {
-            var response = await _apiClient.ListSharedPublicationVersionsAsync(
-                BuildSettings(),
-                shareName,
-                limit,
-                _shutdown.Token);
-
-            if (requestVersion != sharedPublicationVersionsLoadVersion)
-            {
-                return;
-            }
-
-            if (response.Ok && response.Data is not null)
-            {
-                SharedPublicationVersions.Clear();
-                foreach (var publication in response.Data)
-                {
-                    SharedPublicationVersions.Add(
-                        new SharedPublicationListItemViewModel(publication, DisplayTextFormatter));
-                }
-
-                SharedPublicationVersionMessage =
-                    F(
-                        "format.loaded_shared_publication_versions",
-                        SharedPublicationVersions.Count,
-                        shareName);
-                return;
-            }
-
-            SharedPublicationVersionMessage = T("data.shared_publication_versions_refresh_failed");
-            SharedPublicationVersionErrorMessage = DescribeError(response);
-        }
-        finally
-        {
-            if (requestVersion == sharedPublicationVersionsLoadVersion)
-            {
-                IsLoadingSharedPublicationVersions = false;
-            }
-        }
-    }
 
     partial void OnSelectedSharedPublicationChanged(SharedPublicationListItemViewModel? value)
     {
