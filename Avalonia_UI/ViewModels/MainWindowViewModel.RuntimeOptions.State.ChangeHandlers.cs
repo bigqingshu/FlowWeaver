c5276111de37d200b15357eda53162af1ed83061
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 00:21:21 +0800

    拆分运行配置草稿变更回调

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.State.ChangeHandlers.cs
@@ -0,0 +1,83 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    partial void OnRuntimeOptionsProfileDraftChanged(string value)
+    {
+        NotifyRuntimeOptionsSummaryChanged();
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsStrictValidationDraftChanged(bool value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsLogLevelDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsEventLevelDraftChanged(string value)
+    {
+        NotifyRuntimeOptionsSummaryChanged();
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsEventRateLimitPerSecondDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsProgressEnabledDraftChanged(bool value)
+    {
+        NotifyRuntimeOptionsSummaryChanged();
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsProgressIntervalSecondsDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsCaptureErrorContextDraftChanged(bool value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsIncludeMetricsDraftChanged(bool value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsPayloadByteLimitDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsTtlSecondsDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsRedactColumnsDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsMaskPolicyDraftChanged(string value)
+    {
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsNodeOverrideCountChanged(int value)
+    {
+        NotifyRuntimeOptionsSummaryChanged();
+        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
+    }
+
+    partial void OnRuntimeOptionsEditorErrorMessageChanged(string? value)
+    {
+        OnPropertyChanged(nameof(HasRuntimeOptionsEditorError));
+    }
+}
