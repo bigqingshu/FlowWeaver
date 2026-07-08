565f51b7c46ea60af8c15ce907e50c2b33cb5be7
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 16:23:26 +0800

    拆分运行配置节点状态变更辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.State.Changes.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.State.Changes.cs
@@ -1,17 +1,7 @@
-using Avalonia_UI.Models;
-
 namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    partial void OnSelectedRuntimeOptionsNodeChanged(
-        WorkflowDefinitionNodeListItemViewModel? value)
-    {
-        OnPropertyChanged(nameof(HasSelectedRuntimeOptionsNode));
-        RefreshSelectedRuntimeOptionsNodeDraftState();
-        ResetRuntimeOptionsSelectedNodeOverrideCommand.NotifyCanExecuteChanged();
-    }
-
     partial void OnRuntimeOptionsProfileDraftChanged(string value)
     {
         NotifyRuntimeOptionsSummaryChanged();
@@ -76,73 +66,6 @@ public partial class MainWindowViewModel
     }
 
     partial void OnRuntimeOptionsMaskPolicyDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeProfileDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeStrictValidationDraftChanged(bool value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeLogLevelDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeEventLevelDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeEventRateLimitPerSecondDraftChanged(
-        string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeProgressEnabledDraftChanged(bool value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeProgressIntervalSecondsDraftChanged(
-        string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeCaptureErrorContextDraftChanged(bool value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeIncludeMetricsDraftChanged(bool value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodePayloadByteLimitDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeTtlSecondsDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeRedactColumnsDraftChanged(string value)
-    {
-        RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
-    }
-
-    partial void OnRuntimeOptionsSelectedNodeMaskPolicyDraftChanged(string value)
     {
         RefreshRuntimeOptionsJsonDraftFromStructuredInputsIfClean();
     }
