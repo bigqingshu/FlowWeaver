2c83be89ad490229ab57bf472dcf3c8be67cd692
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 20:10:10 +0800

    拆分运行配置工作流草稿字段

--- Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.State.Changes.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.State.Changes.cs
@@ -1,7 +1,49 @@
+using Avalonia_UI.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
+    [ObservableProperty]
+    private string runtimeOptionsProfileDraft = RuntimeOptionsDefaults.Profile;
+
+    [ObservableProperty]
+    private bool runtimeOptionsStrictValidationDraft = true;
+
+    [ObservableProperty]
+    private string runtimeOptionsLogLevelDraft = RuntimeOptionsDefaults.LogLevel;
+
+    [ObservableProperty]
+    private string runtimeOptionsEventLevelDraft = RuntimeOptionsDefaults.EventLevel;
+
+    [ObservableProperty]
+    private string runtimeOptionsEventRateLimitPerSecondDraft = "0";
+
+    [ObservableProperty]
+    private bool runtimeOptionsProgressEnabledDraft = true;
+
+    [ObservableProperty]
+    private string runtimeOptionsProgressIntervalSecondsDraft = "0";
+
+    [ObservableProperty]
+    private bool runtimeOptionsCaptureErrorContextDraft = true;
+
+    [ObservableProperty]
+    private bool runtimeOptionsIncludeMetricsDraft = true;
+
+    [ObservableProperty]
+    private string runtimeOptionsPayloadByteLimitDraft = "0";
+
+    [ObservableProperty]
+    private string runtimeOptionsTtlSecondsDraft = "0";
+
+    [ObservableProperty]
+    private string runtimeOptionsRedactColumnsDraft = string.Empty;
+
+    [ObservableProperty]
+    private string runtimeOptionsMaskPolicyDraft = RuntimeOptionsDefaults.MaskPolicy;
+
     partial void OnRuntimeOptionsProfileDraftChanged(string value)
     {
         NotifyRuntimeOptionsSummaryChanged();
--- Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.cs
@@ -14,45 +14,6 @@ namespace Avalonia_UI.ViewModels;
 public partial class MainWindowViewModel
 {
     [ObservableProperty]
-    private string runtimeOptionsProfileDraft = RuntimeOptionsDefaults.Profile;
-
-    [ObservableProperty]
-    private bool runtimeOptionsStrictValidationDraft = true;
-
-    [ObservableProperty]
-    private string runtimeOptionsLogLevelDraft = RuntimeOptionsDefaults.LogLevel;
-
-    [ObservableProperty]
-    private string runtimeOptionsEventLevelDraft = RuntimeOptionsDefaults.EventLevel;
-
-    [ObservableProperty]
-    private string runtimeOptionsEventRateLimitPerSecondDraft = "0";
-
-    [ObservableProperty]
-    private bool runtimeOptionsProgressEnabledDraft = true;
-
-    [ObservableProperty]
-    private string runtimeOptionsProgressIntervalSecondsDraft = "0";
-
-    [ObservableProperty]
-    private bool runtimeOptionsCaptureErrorContextDraft = true;
-
-    [ObservableProperty]
-    private bool runtimeOptionsIncludeMetricsDraft = true;
-
-    [ObservableProperty]
-    private string runtimeOptionsPayloadByteLimitDraft = "0";
-
-    [ObservableProperty]
-    private string runtimeOptionsTtlSecondsDraft = "0";
-
-    [ObservableProperty]
-    private string runtimeOptionsRedactColumnsDraft = string.Empty;
-
-    [ObservableProperty]
-    private string runtimeOptionsMaskPolicyDraft = RuntimeOptionsDefaults.MaskPolicy;
-
-    [ObservableProperty]
     private WorkflowDefinitionNodeListItemViewModel? selectedRuntimeOptionsNode;
 
     [ObservableProperty]
