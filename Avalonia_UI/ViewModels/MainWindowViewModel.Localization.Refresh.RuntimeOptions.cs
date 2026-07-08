c631a749fb1232e3c7f9b4f6ee7b5c4036deb9ce
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 22:10:00 +0800

    拆分运行配置本地化刷新通知

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.Localization.Refresh.RuntimeOptions.cs
@@ -0,0 +1,38 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    private void NotifyRuntimeOptionsLocalizedTextChanged()
+    {
+        OnPropertyChanged(nameof(RuntimeOptionsSectionText));
+        OnPropertyChanged(nameof(RuntimeOptionsOpenEditorText));
+        OnPropertyChanged(nameof(RuntimeOptionsWindowTitleText));
+        OnPropertyChanged(nameof(RuntimeOptionsWorkflowSectionText));
+        OnPropertyChanged(nameof(RuntimeOptionsNodeOverrideSectionText));
+        OnPropertyChanged(nameof(RuntimeOptionsProfileText));
+        OnPropertyChanged(nameof(RuntimeOptionsProfileOptions));
+        OnPropertyChanged(nameof(RuntimeOptionsStrictValidationText));
+        OnPropertyChanged(nameof(RuntimeOptionsLogLevelText));
+        OnPropertyChanged(nameof(RuntimeOptionsLogLevelOptions));
+        OnPropertyChanged(nameof(RuntimeOptionsEventLevelText));
+        OnPropertyChanged(nameof(RuntimeOptionsEventLevelOptions));
+        OnPropertyChanged(nameof(RuntimeOptionsEventRateLimitText));
+        OnPropertyChanged(nameof(RuntimeOptionsProgressEnabledText));
+        OnPropertyChanged(nameof(RuntimeOptionsProgressIntervalText));
+        OnPropertyChanged(nameof(RuntimeOptionsCaptureErrorContextText));
+        OnPropertyChanged(nameof(RuntimeOptionsIncludeMetricsText));
+        OnPropertyChanged(nameof(RuntimeOptionsPayloadByteLimitText));
+        OnPropertyChanged(nameof(RuntimeOptionsTtlSecondsText));
+        OnPropertyChanged(nameof(RuntimeOptionsRedactColumnsText));
+        OnPropertyChanged(nameof(RuntimeOptionsMaskPolicyText));
+        OnPropertyChanged(nameof(RuntimeOptionsMaskPolicyOptions));
+        OnPropertyChanged(nameof(RuntimeOptionsJsonSectionText));
+        OnPropertyChanged(nameof(RuntimeOptionsJsonRegenerateText));
+        OnPropertyChanged(nameof(RuntimeOptionsJsonWatermarkText));
+        OnPropertyChanged(nameof(RuntimeOptionsSelectNodeText));
+        OnPropertyChanged(nameof(RuntimeOptionsApplyText));
+        OnPropertyChanged(nameof(RuntimeOptionsResetNodeOverrideText));
+        NotifyRuntimeOptionsSummaryChanged();
+        OnPropertyChanged(nameof(SelectedRunRuntimeOptionsSummaryText));
+    }
+}
