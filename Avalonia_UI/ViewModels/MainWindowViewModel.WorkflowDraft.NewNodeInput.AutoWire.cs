e5a8c747e4d04e44fae0a625cfa31567448c06cf
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 01:19:06 +0800

    拆分新增节点自动连线端口选择

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NewNodeInput.AutoWire.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NewNodeInput.AutoWire.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Avalonia_UI.Api;
 
 namespace Avalonia_UI.ViewModels;
 
@@ -38,29 +35,5 @@ public partial class MainWindowViewModel
         return sourceDefinition is null
             ? null
             : TryGetPreferredOutputPort(sourceDefinition.OutputPorts);
-    }
-
-    private static string? TryGetSingleInputPort(
-        IReadOnlyList<NodePortDefinitionDto> inputPorts)
-    {
-        return inputPorts.Count == 1 ? inputPorts[0].Name : null;
-    }
-
-    private static string? TryGetPreferredOutputPort(
-        IReadOnlyList<NodePortDefinitionDto> outputPorts)
-    {
-        if (outputPorts.Count == 0)
-        {
-            return null;
-        }
-
-        var outPort = outputPorts.FirstOrDefault(port =>
-            string.Equals(port.Name, "out", StringComparison.Ordinal));
-        if (outPort is not null)
-        {
-            return outPort.Name;
-        }
-
-        return outputPorts.Count == 1 ? outputPorts[0].Name : null;
     }
 }
