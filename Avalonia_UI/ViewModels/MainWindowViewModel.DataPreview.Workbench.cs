031afad5b6499e36279159d6f99871cf034a8dcf
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 10:38:44 +0800

    拆分数据预览工作台可执行条件辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.cs
@@ -9,54 +9,6 @@ namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    private bool CanLoadSelectedDataPreviewTable()
-    {
-        return CanUseEngineActions
-            && SelectedDataPreviewTableOption is not null
-            && !IsLoadingDataPreviewWorkbench;
-    }
-
-    private bool CanLoadPreviousDataPreviewWorkbenchPage()
-    {
-        return CanUseEngineActions
-            && LoadedDataPreviewTableRef is not null
-            && !IsLoadingDataPreviewWorkbench
-            && dataPreviewWorkbenchOffset > 0;
-    }
-
-    private bool CanLoadNextDataPreviewWorkbenchPage()
-    {
-        return CanUseEngineActions
-            && LoadedDataPreviewTableRef is not null
-            && !IsLoadingDataPreviewWorkbench
-            && dataPreviewWorkbenchHasMore;
-    }
-
-    private bool CanCopyDataPreviewWorkbenchTsv()
-    {
-        return HasDataPreviewWorkbenchColumns
-            && HasDataPreviewWorkbenchRows;
-    }
-
-    private bool CanParseDataPreviewWorkbenchPaste()
-    {
-        return HasDataPreviewWorkbenchPasteText
-            && !IsLoadingDataPreviewWorkbench;
-    }
-
-    private bool CanRestoreDataPreviewWorkbenchDraft()
-    {
-        return IsDataPreviewWorkbenchDirty
-            && !IsLoadingDataPreviewWorkbench;
-    }
-
-    private bool CanSaveDataPreviewWorkbenchAs()
-    {
-        return IsDataPreviewWorkbenchDirty
-            && CanSaveDataPreviewWorkbenchAsDraft
-            && !IsLoadingDataPreviewWorkbench;
-    }
-
     [RelayCommand(CanExecute = nameof(CanLoadSelectedDataPreviewTable))]
     private async Task LoadSelectedDataPreviewTableAsync()
     {
