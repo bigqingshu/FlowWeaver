bd84501228e20598bd3440948bd36189500b4d9c
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 14:35:48 +0800

    拆分节点目录刷新辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Catalog.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Catalog.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Threading.Tasks;
 using Avalonia_UI.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -79,103 +78,6 @@ public partial class MainWindowViewModel
     private async Task RefreshNodeDefinitionsAsync()
     {
         await RefreshNodeDefinitionsCoreAsync(allowStateCacheHit: false);
-    }
-
-    private async Task RefreshNodeDefinitionsCoreAsync(bool allowStateCacheHit)
-    {
-        var requestVersion = ++nodeDefinitionsLoadVersion;
-        IsLoadingNodeDefinitions = true;
-        NodeDefinitionCatalogMessage = T("node_catalog.loading");
-        NodeDefinitionCatalogErrorMessage = null;
-        var settings = BuildSettings();
-        var connectionKey = NodeDefinitionCatalogCacheState.BuildConnectionKey(settings);
-
-        try
-        {
-            var catalogState = await TryGetNodeDefinitionCatalogStateAsync(settings);
-            if (requestVersion != nodeDefinitionsLoadVersion)
-            {
-                return;
-            }
-
-            if (allowStateCacheHit
-                && nodeDefinitionCatalogCacheState.IsCatalogHit(connectionKey, catalogState))
-            {
-                NodeDefinitionCatalogMessage =
-                    F("format.loaded_node_definitions", NodeDefinitions.Count);
-                OnPropertyChanged(nameof(HasNodeDefinitions));
-                OnPropertyChanged(nameof(HasNodeDefinitionCatalogEmptyState));
-                return;
-            }
-
-            var response = await _apiClient.ListNodeDefinitionsAsync(
-                settings,
-                _shutdown.Token);
-
-            if (requestVersion != nodeDefinitionsLoadVersion)
-            {
-                return;
-            }
-
-            if (response.Ok && response.Data is not null)
-            {
-                SelectedNewDraftNodeDefinition = null;
-                NodeDefinitions.Clear();
-                nodeDefinitionByKey.Clear();
-                var schemaCatalogKey = PrepareNodeConfigSchemaCache(
-                    connectionKey,
-                    catalogState);
-                foreach (var definition in response.Data
-                    .OrderBy(definition => definition.DisplayName)
-                    .ThenBy(definition => definition.NodeType)
-                    .ThenBy(definition => definition.NodeVersion))
-                {
-                    var item = new NodeDefinitionListItemViewModel(
-                        definition,
-                        DisplayTextFormatter,
-                        GetOrParseNodeConfigSchema(definition, schemaCatalogKey));
-                    NodeDefinitions.Add(item);
-                    nodeDefinitionByKey[NodeDefinitionCatalogCacheState.BuildLookupKey(
-                            item.NodeType,
-                            item.NodeVersion)] =
-                        item;
-                }
-
-                nodeDefinitionCatalogCacheState.RecordLoadedCatalog(connectionKey, catalogState);
-                RefreshNodeEditorSchemaFallbackNodes();
-                OnPropertyChanged(nameof(HasNodeDefinitions));
-                OnPropertyChanged(nameof(HasNodeDefinitionCatalogEmptyState));
-                RefreshWorkflowDefinitionDraftStructureState();
-                RefreshSelectedNodeConfigDraftState();
-                NodeDefinitionCatalogMessage =
-                    F("format.loaded_node_definitions", NodeDefinitions.Count);
-                return;
-            }
-
-            NodeDefinitionCatalogMessage = T("node_catalog.refresh_failed");
-            NodeDefinitionCatalogErrorMessage = DescribeError(response);
-            SelectedNewDraftNodeDefinition = null;
-            OnPropertyChanged(nameof(HasNodeDefinitions));
-            OnPropertyChanged(nameof(HasNodeDefinitionCatalogEmptyState));
-            RefreshSelectedNodeConfigDraftState();
-        }
-        finally
-        {
-            if (requestVersion == nodeDefinitionsLoadVersion)
-            {
-                IsLoadingNodeDefinitions = false;
-            }
-        }
-    }
-
-    private async Task RefreshNodeDefinitionsAfterHealthyConnectionAsync()
-    {
-        if (!CanRefreshNodeDefinitions())
-        {
-            return;
-        }
-
-        await RefreshNodeDefinitionsCoreAsync(allowStateCacheHit: true);
     }
 
     partial void OnIsLoadingNodeDefinitionsChanged(bool value)
