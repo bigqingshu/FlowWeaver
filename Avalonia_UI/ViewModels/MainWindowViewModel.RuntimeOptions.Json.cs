5792ddb0e1839f9b35ff70a03c4effd24000872d
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 20:22:26 +0800

    拆分运行配置 JSON 草稿状态字段

--- Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.Json.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.Json.cs
@@ -1,10 +1,22 @@
 using Avalonia_UI.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
+    [ObservableProperty]
+    private bool isRuntimeOptionsJsonEditorExpanded;
+
+    [ObservableProperty]
+    private string runtimeOptionsJsonDraft = string.Empty;
+
+    [ObservableProperty]
+    private bool isRuntimeOptionsJsonDraftDirty;
+
+    private bool isSynchronizingRuntimeOptionsJsonDraft;
+
     public string RuntimeOptionsJsonSectionText =>
         T("definition.runtime_options_json_section");
 
--- Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.cs
@@ -13,15 +13,4 @@ namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    [ObservableProperty]
-    private bool isRuntimeOptionsJsonEditorExpanded;
-
-    [ObservableProperty]
-    private string runtimeOptionsJsonDraft = string.Empty;
-
-    [ObservableProperty]
-    private bool isRuntimeOptionsJsonDraftDirty;
-
-    private bool isSynchronizingRuntimeOptionsJsonDraft;
-
 }
