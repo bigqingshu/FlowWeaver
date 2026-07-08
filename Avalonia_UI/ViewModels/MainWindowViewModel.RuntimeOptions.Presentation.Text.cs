861fa53cf36fe2c9dd7fc9b31ab5ae11049170b7
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 07:02:12 +0800

    拆分运行配置展示文案属性

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.RuntimeOptions.Presentation.Text.cs
@@ -0,0 +1,63 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    public string RuntimeOptionsSectionText => T("definition.runtime_options");
+
+    public string RuntimeOptionsOpenEditorText =>
+        T("definition.runtime_options_open_editor");
+
+    public string RuntimeOptionsWindowTitleText =>
+        T("definition.runtime_options_window_title");
+
+    public string RuntimeOptionsWorkflowSectionText =>
+        T("definition.runtime_options_workflow_section");
+
+    public string RuntimeOptionsNodeOverrideSectionText =>
+        T("definition.runtime_options_node_override_section");
+
+    public string RuntimeOptionsProfileText => T("definition.runtime_options_profile");
+
+    public string RuntimeOptionsStrictValidationText =>
+        T("definition.runtime_options_strict_validation");
+
+    public string RuntimeOptionsLogLevelText => T("definition.runtime_options_log_level");
+
+    public string RuntimeOptionsEventLevelText =>
+        T("definition.runtime_options_event_level");
+
+    public string RuntimeOptionsEventRateLimitText =>
+        T("definition.runtime_options_event_rate_limit");
+
+    public string RuntimeOptionsProgressEnabledText =>
+        T("definition.runtime_options_progress_enabled");
+
+    public string RuntimeOptionsProgressIntervalText =>
+        T("definition.runtime_options_progress_interval");
+
+    public string RuntimeOptionsCaptureErrorContextText =>
+        T("definition.runtime_options_capture_error_context");
+
+    public string RuntimeOptionsIncludeMetricsText =>
+        T("definition.runtime_options_include_metrics");
+
+    public string RuntimeOptionsPayloadByteLimitText =>
+        T("definition.runtime_options_payload_byte_limit");
+
+    public string RuntimeOptionsTtlSecondsText =>
+        T("definition.runtime_options_ttl_seconds");
+
+    public string RuntimeOptionsRedactColumnsText =>
+        T("definition.runtime_options_redact_columns");
+
+    public string RuntimeOptionsMaskPolicyText =>
+        T("definition.runtime_options_mask_policy");
+
+    public string RuntimeOptionsSelectNodeText =>
+        T("definition.runtime_options_select_node");
+
+    public string RuntimeOptionsApplyText => T("definition.runtime_options_apply");
+
+    public string RuntimeOptionsResetNodeOverrideText =>
+        T("definition.runtime_options_reset_node_override");
+}
