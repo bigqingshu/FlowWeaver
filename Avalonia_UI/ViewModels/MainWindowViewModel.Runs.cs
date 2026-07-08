cde2f3a55fc6a86a00d55f9397af30cdb1ca33b0
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 12:03:45 +0800

    拆分运行选择状态辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.Runs.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.Runs.cs
@@ -255,46 +255,6 @@ public partial class MainWindowViewModel
         NotifyRunCommandStateChanged();
     }
 
-    partial void OnSelectedRunChanged(
-        WorkflowRunListItemViewModel? oldValue,
-        WorkflowRunListItemViewModel? newValue)
-    {
-        var runChanged = !string.Equals(
-            oldValue?.WorkflowRunId,
-            newValue?.WorkflowRunId,
-            StringComparison.Ordinal);
-        if (runChanged)
-        {
-            nodeRunsLoadVersion++;
-            tableRefsLoadVersion++;
-            IsLoadingNodeRuns = false;
-            IsLoadingTableRefs = false;
-            NodeRuns.Clear();
-            TableRefs.Clear();
-            NodeRunMessage = newValue is null
-                ? T("status.select_run_node_status")
-                : F("format.selected_run_refresh_nodes", newValue.WorkflowRunId);
-            NodeRunErrorMessage = null;
-            TableRefMessage = newValue is null
-                ? T("status.select_run_table_refs")
-                : F("format.selected_run_refresh_table_refs", newValue.WorkflowRunId);
-            TableRefErrorMessage = null;
-            ResetDataPreviewSelectionState();
-            ResetDataPreviewWorkbenchState();
-        }
-        else
-        {
-            RefreshSelectedWorkflowNodeDataPreviewCommand.NotifyCanExecuteChanged();
-        }
-
-        NotifyEngineActionStateChanged();
-        OnPropertyChanged(nameof(HasSelectedRunRuntimeOptionsSummary));
-        OnPropertyChanged(nameof(SelectedRunRuntimeOptionsSummaryText));
-        RefreshNodeRunsCommand.NotifyCanExecuteChanged();
-        RefreshTableRefsCommand.NotifyCanExecuteChanged();
-        LoadSelectedDataPreviewTableCommand.NotifyCanExecuteChanged();
-    }
-
     partial void OnRunErrorMessageChanged(string? value)
     {
         OnPropertyChanged(nameof(HasRunError));
