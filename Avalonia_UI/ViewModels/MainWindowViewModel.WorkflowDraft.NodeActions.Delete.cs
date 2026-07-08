3f577625a8beaccaccadc51a45929e86917d9efe
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 01:00:36 +0800

    拆分批量删除节点命令

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.Delete.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.Delete.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Avalonia_UI.Models;
 using CommunityToolkit.Mvvm.Input;
 
@@ -43,46 +42,6 @@ public partial class MainWindowViewModel
                 : FormatRemovedConnectionsMessage(patchResult.RemovedConnections);
         ShowWorkflowDefinitionNotification(
             "workflow.definition.delete_node",
-            UiNotificationKind.Success);
-    }
-
-    [RelayCommand(CanExecute = nameof(CanDeleteSelectedWorkflowDefinitionDraftNodes))]
-    private void DeleteSelectedWorkflowDefinitionDraftNodes()
-    {
-        var selectedNodeIds = WorkflowDefinitionDraftNodes
-            .Where(node => node.IsBatchSelected)
-            .Select(node => node.NodeInstanceId)
-            .ToArray();
-        if (selectedNodeIds.Length == 0)
-        {
-            return;
-        }
-
-        var patchResult = WorkflowDefinitionDraftNodePatcher.DeleteNodes(
-            WorkflowDefinitionDraftJson,
-            selectedNodeIds);
-        if (!patchResult.Succeeded)
-        {
-            WorkflowDefinitionValidationMessage = T("definition.node_delete_failed");
-            WorkflowDefinitionValidationErrorMessage =
-                LocalizeWorkflowDefinitionDraftWarning(patchResult.Warning);
-            ShowWorkflowDefinitionNotification(
-                "workflow.definition.delete_nodes",
-                UiNotificationKind.Error);
-            return;
-        }
-
-        WorkflowDefinitionDraftJson = patchResult.UpdatedWorkflowDefinitionDraftJson;
-        WorkflowDefinitionValidationMessage =
-            patchResult.RemovedConnections.Count > 0
-                ? F(
-                    "format.workflow_definition_nodes_deleted_with_connections",
-                    selectedNodeIds.Length)
-                : F("format.workflow_definition_nodes_deleted", selectedNodeIds.Length);
-        WorkflowDefinitionValidationErrorMessage =
-            FormatRemovedConnectionsMessage(patchResult.RemovedConnections);
-        ShowWorkflowDefinitionNotification(
-            "workflow.definition.delete_nodes",
             UiNotificationKind.Success);
     }
 }
