1b59384010949e1a3b5f0f973f8fd7093e6f370e
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 01:48:25 +0800

    拆分删除节点重连提示

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeConnectionMessages.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeConnectionMessages.cs
@@ -22,30 +22,4 @@ public partial class MainWindowViewModel
                 removedConnections.Select(FormatRelatedConnectionSummary)));
     }
 
-    private string? FormatDeletedRewiredConnectionsMessage(
-        IReadOnlyList<WorkflowDefinitionDraftConnection> removedConnections,
-        IReadOnlyList<WorkflowDefinitionDraftConnection> addedConnections)
-    {
-        if (removedConnections.Count == 0 && addedConnections.Count == 0)
-        {
-            return null;
-        }
-
-        var removedText = removedConnections.Count == 0
-            ? "-"
-            : string.Join(
-                Environment.NewLine,
-                removedConnections.Select(FormatRelatedConnectionSummary));
-        var addedText = addedConnections.Count == 0
-            ? "-"
-            : string.Join(
-                Environment.NewLine,
-                addedConnections.Select(FormatRelatedConnectionSummary));
-
-        return F(
-            "definition.node_delete_rewired_connections",
-            removedText,
-            addedText);
-    }
-
 }
