444dc3759fcd4f372f057735d28802cb97cd9cb6
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 00:52:07 +0800

    拆分删除节点可执行判断

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.CanExecute.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.CanExecute.cs
@@ -1,32 +1,7 @@
-using System.Linq;
-
 namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    private bool CanDeleteWorkflowDefinitionDraftNode()
-    {
-        return CanUseEngineActions
-            && WorkflowDefinitionDetail is not null
-            && SelectedWorkflowDefinitionNode is not null
-            && HasWorkflowDefinitionDraft
-            && !IsWorkflowDefinitionDraftBusy
-            && !HasWorkflowDefinitionRevisionConflict
-            && FindDraftNode(SelectedWorkflowDefinitionNode.NodeInstanceId) is not null;
-    }
-
-    private bool CanDeleteSelectedWorkflowDefinitionDraftNodes()
-    {
-        return CanUseEngineActions
-            && WorkflowDefinitionDetail is not null
-            && HasWorkflowDefinitionDraft
-            && !IsWorkflowDefinitionDraftBusy
-            && !HasWorkflowDefinitionRevisionConflict
-            && WorkflowDefinitionDraftNodes.Any(node =>
-                node.IsBatchSelected
-                && FindDraftNode(node.NodeInstanceId) is not null);
-    }
-
     private bool CanCopyWorkflowDefinitionDraftNode()
     {
         return CanUseEngineActions
