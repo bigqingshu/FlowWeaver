3a67de2eb666ddfa5e3c4a5468ff909ba3e4f0d6
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 09:28:02 +0800

    拆分新增节点操作辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-using System.Text.Json;
 using Avalonia_UI.Models;
 using CommunityToolkit.Mvvm.Input;
 
@@ -8,65 +7,6 @@ namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    [RelayCommand(CanExecute = nameof(CanAddWorkflowDefinitionDraftNode))]
-    private void AddWorkflowDefinitionDraftNode()
-    {
-        var autoWirePorts = TryGetAutoWirePorts();
-        JsonElement config;
-        try
-        {
-            using var parsed = JsonDocument.Parse(NewDraftNodeConfigJson);
-            config = parsed.RootElement.Clone();
-        }
-        catch (JsonException)
-        {
-            WorkflowDefinitionValidationMessage = T("definition.node_add_failed");
-            WorkflowDefinitionValidationErrorMessage =
-                T("definition.node_add_config_json_invalid");
-            ShowWorkflowDefinitionNotification(
-                "workflow.definition.add_node",
-                UiNotificationKind.Error);
-            return;
-        }
-
-        var patchResult = WorkflowDefinitionDraftNodePatcher.AddNode(
-            WorkflowDefinitionDraftJson,
-            NewDraftNodeInstanceId,
-            NewDraftNodeType,
-            NewDraftNodeVersion,
-            NewDraftNodeDisplayName,
-            config,
-            SelectedWorkflowDefinitionNode?.NodeInstanceId,
-            autoWirePorts.InputPort,
-            autoWirePorts.OutputPort,
-            autoWirePorts.SourceOutputPort);
-        if (!patchResult.Succeeded)
-        {
-            WorkflowDefinitionValidationMessage = T("definition.node_add_failed");
-            WorkflowDefinitionValidationErrorMessage =
-                LocalizeWorkflowDefinitionDraftWarning(patchResult.Warning);
-            ShowWorkflowDefinitionNotification(
-                "workflow.definition.add_node",
-                UiNotificationKind.Error);
-            return;
-        }
-
-        WorkflowDefinitionDraftJson = patchResult.UpdatedWorkflowDefinitionDraftJson;
-        SelectWorkflowDefinitionDraftNode(NewDraftNodeInstanceId);
-        WorkflowDefinitionValidationMessage =
-            patchResult.AddedConnections.Count > 0
-                ? T("definition.node_added_with_connections")
-                : T("definition.node_added");
-        WorkflowDefinitionValidationErrorMessage =
-            FormatAutoWiredConnectionsMessage(
-                patchResult.RemovedConnections,
-                patchResult.AddedConnections);
-        ShowWorkflowDefinitionNotification(
-            "workflow.definition.add_node",
-            UiNotificationKind.Success);
-        ResetNewDraftNodeInput();
-    }
-
     [RelayCommand(CanExecute = nameof(CanDeleteWorkflowDefinitionDraftNode))]
     private void DeleteWorkflowDefinitionDraftNode()
     {
