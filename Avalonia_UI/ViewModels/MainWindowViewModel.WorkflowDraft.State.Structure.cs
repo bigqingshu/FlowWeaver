f28c55e4bb0f100019b46f10b78addfd7277bb34
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 18:34:03 +0800

    拆分工作流线性链状态文本属性

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Structure.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Structure.cs
@@ -14,6 +14,31 @@ public partial class MainWindowViewModel
         ClearSelectedNewDraftConnectionNodesIfMissing();
     }
 
+    public string WorkflowLinearChainStatusText
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(WorkflowDefinitionDraftJson))
+            {
+                return T("definition.linear_chain_status_not_loaded");
+            }
+
+            var analysis = ReadWorkflowDefinitionLinearChainAnalysisFromCache();
+            if (analysis is null)
+            {
+                return T("definition.linear_chain_status_not_loaded");
+            }
+
+            return analysis.IsLinear
+                ? F(
+                    "definition.linear_chain_status_linear",
+                    analysis.NodeInstanceIds.Count)
+                : F(
+                    "definition.linear_chain_status_not_linear",
+                    LocalizeWorkflowDefinitionDraftWarning(analysis.Warning));
+        }
+    }
+
     partial void OnWorkflowDefinitionDraftStructureChanged(
         WorkflowDefinitionDraftStructure? value)
     {
--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.cs
@@ -165,31 +165,6 @@ public partial class MainWindowViewModel
 
     public string NodeMoveSemanticsText => T("definition.node_move_semantics");
 
-    public string WorkflowLinearChainStatusText
-    {
-        get
-        {
-            if (string.IsNullOrWhiteSpace(WorkflowDefinitionDraftJson))
-            {
-                return T("definition.linear_chain_status_not_loaded");
-            }
-
-            var analysis = ReadWorkflowDefinitionLinearChainAnalysisFromCache();
-            if (analysis is null)
-            {
-                return T("definition.linear_chain_status_not_loaded");
-            }
-
-            return analysis.IsLinear
-                ? F(
-                    "definition.linear_chain_status_linear",
-                    analysis.NodeInstanceIds.Count)
-                : F(
-                    "definition.linear_chain_status_not_linear",
-                    LocalizeWorkflowDefinitionDraftWarning(analysis.Warning));
-        }
-    }
-
     public string NodeInstanceIdText => T("definition.node_instance_id");
 
     public string NodeTypeText => T("definition.node_type");
