f5b1100dd0ce60ef2f64cfc01fc8cb5f77ee0639
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 01:34:13 +0800

    拆分移动节点重连提示

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeConnectionMessages.Move.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia_UI.Models;
+
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    private string? FormatMovedRewiredConnectionsMessage(
+        IReadOnlyList<WorkflowDefinitionDraftConnection> removedConnections,
+        IReadOnlyList<WorkflowDefinitionDraftConnection> addedConnections)
+    {
+        if (removedConnections.Count == 0 && addedConnections.Count == 0)
+        {
+            return null;
+        }
+
+        var removedText = removedConnections.Count == 0
+            ? "-"
+            : string.Join(
+                Environment.NewLine,
+                removedConnections.Select(FormatRelatedConnectionSummary));
+        var addedText = addedConnections.Count == 0
+            ? "-"
+            : string.Join(
+                Environment.NewLine,
+                addedConnections.Select(FormatRelatedConnectionSummary));
+
+        return F(
+            "definition.node_move_rewired_connections",
+            removedText,
+            addedText);
+    }
+}
--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeConnectionMessages.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeConnectionMessages.cs
@@ -74,29 +74,4 @@ public partial class MainWindowViewModel
             addedText);
     }
 
-    private string? FormatMovedRewiredConnectionsMessage(
-        IReadOnlyList<WorkflowDefinitionDraftConnection> removedConnections,
-        IReadOnlyList<WorkflowDefinitionDraftConnection> addedConnections)
-    {
-        if (removedConnections.Count == 0 && addedConnections.Count == 0)
-        {
-            return null;
-        }
-
-        var removedText = removedConnections.Count == 0
-            ? "-"
-            : string.Join(
-                Environment.NewLine,
-                removedConnections.Select(FormatRelatedConnectionSummary));
-        var addedText = addedConnections.Count == 0
-            ? "-"
-            : string.Join(
-                Environment.NewLine,
-                addedConnections.Select(FormatRelatedConnectionSummary));
-
-        return F(
-            "definition.node_move_rewired_connections",
-            removedText,
-            addedText);
-    }
 }
