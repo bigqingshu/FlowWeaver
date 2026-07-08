dcf480bfe14359395f8d9141aa48306b903cdafe
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 09:44:03 +0800

    拆分移动节点操作辅助文件

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.NodeActions.cs
@@ -1,62 +1,7 @@
-using Avalonia_UI.Models;
-using CommunityToolkit.Mvvm.Input;
-
 namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    [RelayCommand(CanExecute = nameof(CanMoveSelectedWorkflowDefinitionDraftNodeUp))]
-    private void MoveSelectedWorkflowDefinitionDraftNodeUp()
-    {
-        MoveSelectedWorkflowDefinitionDraftNode(offset: -1);
-    }
-
-    [RelayCommand(CanExecute = nameof(CanMoveSelectedWorkflowDefinitionDraftNodeDown))]
-    private void MoveSelectedWorkflowDefinitionDraftNodeDown()
-    {
-        MoveSelectedWorkflowDefinitionDraftNode(offset: 1);
-    }
-
-    private void MoveSelectedWorkflowDefinitionDraftNode(int offset)
-    {
-        if (SelectedWorkflowDefinitionNode is null)
-        {
-            return;
-        }
-
-        var nodeInstanceId = SelectedWorkflowDefinitionNode.NodeInstanceId;
-        var patchResult = WorkflowDefinitionDraftNodePatcher.MoveNodeWithLinearRewire(
-            WorkflowDefinitionDraftJson,
-            nodeInstanceId,
-            offset);
-        if (!patchResult.Succeeded)
-        {
-            WorkflowDefinitionValidationMessage = T("definition.node_move_failed");
-            WorkflowDefinitionValidationErrorMessage =
-                LocalizeWorkflowDefinitionDraftWarning(patchResult.Warning);
-            ShowWorkflowDefinitionNotification(
-                "workflow.definition.move_node",
-                UiNotificationKind.Error);
-            return;
-        }
-
-        WorkflowDefinitionDraftJson = patchResult.UpdatedWorkflowDefinitionDraftJson;
-        SelectWorkflowDefinitionDraftNode(nodeInstanceId);
-        WorkflowDefinitionValidationMessage =
-            patchResult.AddedConnections.Count > 0
-                ? T("definition.node_moved_with_rewired_connections")
-                : T("definition.node_moved");
-        WorkflowDefinitionValidationErrorMessage =
-            patchResult.AddedConnections.Count > 0
-                ? FormatMovedRewiredConnectionsMessage(
-                    patchResult.RemovedConnections,
-                    patchResult.AddedConnections)
-                : null;
-        ShowWorkflowDefinitionNotification(
-            "workflow.definition.move_node",
-            UiNotificationKind.Success);
-    }
-
     private void NotifyWorkflowDefinitionNodeActionCommandsChanged()
     {
         CopyWorkflowDefinitionDraftNodeCommand.NotifyCanExecuteChanged();
