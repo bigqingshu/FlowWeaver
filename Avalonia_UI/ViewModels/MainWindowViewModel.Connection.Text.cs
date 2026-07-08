0363e7b81924c69471b30b3c58506ac478e11f99
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-08 21:04:11 +0800

    拆分连接文案属性

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.Connection.Text.cs
@@ -0,0 +1,18 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    public string ConnectionBaseUrlText => T("connection.base_url");
+
+    public string ConnectionTokenText => T("connection.token");
+
+    public string ConnectionStatusText => T("connection.status");
+
+    public string ConnectionEventsText => T("connection.events");
+
+    public string CheckConnectionText => T("connection.check");
+
+    public string StreamText => T("connection.stream");
+
+    public string StopText => T("connection.stop");
+}
--- Avalonia_UI/ViewModels/MainWindowViewModel.Connection.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.Connection.cs
@@ -7,19 +7,4 @@ public partial class MainWindowViewModel
 {
     private readonly EngineHostHealthClient _healthClient;
     private readonly IConnectionSettingsStore _connectionSettingsStore;
-
-    public string ConnectionBaseUrlText => T("connection.base_url");
-
-    public string ConnectionTokenText => T("connection.token");
-
-    public string ConnectionStatusText => T("connection.status");
-
-    public string ConnectionEventsText => T("connection.events");
-
-    public string CheckConnectionText => T("connection.check");
-
-    public string StreamText => T("connection.stream");
-
-    public string StopText => T("connection.stop");
-
 }
