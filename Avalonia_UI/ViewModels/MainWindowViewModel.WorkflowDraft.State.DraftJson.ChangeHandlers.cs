ccc52ce2893eaa0b8c635fe4027d1c01c3571b56
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 14:58:37 +0800

    拆分工作流草稿 JSON 变更处理

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.DraftJson.ChangeHandlers.cs
@@ -0,0 +1,35 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    partial void OnWorkflowDefinitionDraftJsonChanged(string value)
+    {
+        OnPropertyChanged(nameof(HasWorkflowDefinitionDraft));
+        RefreshWorkflowDefinitionDraftStructureState();
+        RefreshSelectedNodeConfigDraftState();
+        RefreshRuntimeOptionsDraftState();
+
+        IsWorkflowDefinitionDraftDirty = value != originalWorkflowDefinitionJson;
+
+        if (WorkflowDefinitionValidationMessage == T("definition.draft_valid") ||
+            WorkflowDefinitionValidationMessage == T("definition.draft_has_issues") ||
+            WorkflowDefinitionValidationMessage == T("definition.validation_failed"))
+        {
+            WorkflowDefinitionValidationMessage = T("definition.validation_invalidated");
+            WorkflowDefinitionValidationErrorMessage = null;
+        }
+
+        ValidateWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
+        RestoreWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
+        ApplySelectedNodeDisplayNameDraftCommand.NotifyCanExecuteChanged();
+        ApplySelectedNodeConfigDraftCommand.NotifyCanExecuteChanged();
+        ApplyRuntimeOptionsDraftCommand.NotifyCanExecuteChanged();
+        RegenerateRuntimeOptionsJsonDraftCommand.NotifyCanExecuteChanged();
+        ResetRuntimeOptionsSelectedNodeOverrideCommand.NotifyCanExecuteChanged();
+        AddWorkflowDefinitionDraftNodeCommand.NotifyCanExecuteChanged();
+        NotifyWorkflowDefinitionNodeActionCommandsChanged();
+        AddWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
+        DeleteWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
+        SaveWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
+    }
+}
--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.DraftJson.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.DraftJson.cs
@@ -8,35 +8,4 @@ public partial class MainWindowViewModel
     private string workflowDefinitionDraftJson = string.Empty;
 
     private string originalWorkflowDefinitionJson = string.Empty;
-
-    partial void OnWorkflowDefinitionDraftJsonChanged(string value)
-    {
-        OnPropertyChanged(nameof(HasWorkflowDefinitionDraft));
-        RefreshWorkflowDefinitionDraftStructureState();
-        RefreshSelectedNodeConfigDraftState();
-        RefreshRuntimeOptionsDraftState();
-
-        IsWorkflowDefinitionDraftDirty = value != originalWorkflowDefinitionJson;
-
-        if (WorkflowDefinitionValidationMessage == T("definition.draft_valid") ||
-            WorkflowDefinitionValidationMessage == T("definition.draft_has_issues") ||
-            WorkflowDefinitionValidationMessage == T("definition.validation_failed"))
-        {
-            WorkflowDefinitionValidationMessage = T("definition.validation_invalidated");
-            WorkflowDefinitionValidationErrorMessage = null;
-        }
-
-        ValidateWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
-        RestoreWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
-        ApplySelectedNodeDisplayNameDraftCommand.NotifyCanExecuteChanged();
-        ApplySelectedNodeConfigDraftCommand.NotifyCanExecuteChanged();
-        ApplyRuntimeOptionsDraftCommand.NotifyCanExecuteChanged();
-        RegenerateRuntimeOptionsJsonDraftCommand.NotifyCanExecuteChanged();
-        ResetRuntimeOptionsSelectedNodeOverrideCommand.NotifyCanExecuteChanged();
-        AddWorkflowDefinitionDraftNodeCommand.NotifyCanExecuteChanged();
-        NotifyWorkflowDefinitionNodeActionCommandsChanged();
-        AddWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
-        DeleteWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
-        SaveWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
-    }
 }
