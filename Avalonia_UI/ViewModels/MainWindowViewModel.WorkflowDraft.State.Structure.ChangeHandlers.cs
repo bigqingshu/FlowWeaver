102b34288fd7aa47d04b0b6951b43e8498f9a3f5
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 04:38:35 +0800

    提取工作流草稿结构展示通知

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Structure.ChangeHandlers.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Structure.ChangeHandlers.cs
@@ -7,14 +7,7 @@ public partial class MainWindowViewModel
     partial void OnWorkflowDefinitionDraftStructureChanged(
         WorkflowDefinitionDraftStructure? value)
     {
-        OnPropertyChanged(nameof(HasWorkflowDefinitionDraftStructure));
-        OnPropertyChanged(nameof(WorkflowDefinitionDraftNodeCount));
-        OnPropertyChanged(nameof(WorkflowDefinitionDraftConnectionCount));
-        OnPropertyChanged(nameof(WorkflowDefinitionDraftNodeCountText));
-        OnPropertyChanged(nameof(WorkflowDefinitionBatchSelectedNodeCount));
-        OnPropertyChanged(nameof(WorkflowDefinitionBatchSelectedNodeCountText));
-        OnPropertyChanged(nameof(WorkflowDefinitionDraftConnectionCountText));
-        OnPropertyChanged(nameof(HasWorkflowDefinitionDraftStructureWarnings));
+        NotifyWorkflowDefinitionDraftStructurePresentationChanged();
         if (SelectedRuntimeOptionsNode is not null &&
             !WorkflowDefinitionDraftNodes.Contains(SelectedRuntimeOptionsNode))
         {
---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Structure.Notifications.cs
@@ -0,0 +1,16 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    private void NotifyWorkflowDefinitionDraftStructurePresentationChanged()
+    {
+        OnPropertyChanged(nameof(HasWorkflowDefinitionDraftStructure));
+        OnPropertyChanged(nameof(WorkflowDefinitionDraftNodeCount));
+        OnPropertyChanged(nameof(WorkflowDefinitionDraftConnectionCount));
+        OnPropertyChanged(nameof(WorkflowDefinitionDraftNodeCountText));
+        OnPropertyChanged(nameof(WorkflowDefinitionBatchSelectedNodeCount));
+        OnPropertyChanged(nameof(WorkflowDefinitionBatchSelectedNodeCountText));
+        OnPropertyChanged(nameof(WorkflowDefinitionDraftConnectionCountText));
+        OnPropertyChanged(nameof(HasWorkflowDefinitionDraftStructureWarnings));
+    }
+}
