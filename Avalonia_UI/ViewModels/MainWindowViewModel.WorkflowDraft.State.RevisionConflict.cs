32c86074c408b6893cba095b94290ce7fae5b17c
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 15:17:31 +0800

    拆分工作流修订冲突变更处理

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.RevisionConflict.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.RevisionConflict.cs
@@ -6,21 +6,4 @@ public partial class MainWindowViewModel
 {
     [ObservableProperty]
     private bool hasWorkflowDefinitionRevisionConflict;
-
-    partial void OnHasWorkflowDefinitionRevisionConflictChanged(bool value)
-    {
-        ApplySelectedNodeDisplayNameDraftCommand.NotifyCanExecuteChanged();
-        ApplySelectedNodeConfigDraftCommand.NotifyCanExecuteChanged();
-        ApplyRuntimeOptionsDraftCommand.NotifyCanExecuteChanged();
-        RegenerateRuntimeOptionsJsonDraftCommand.NotifyCanExecuteChanged();
-        ResetRuntimeOptionsSelectedNodeOverrideCommand.NotifyCanExecuteChanged();
-        AddWorkflowDefinitionDraftNodeCommand.NotifyCanExecuteChanged();
-        NotifyWorkflowDefinitionNodeActionCommandsChanged();
-        AddWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
-        DeleteWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
-        SaveWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
-        StartSelectedWorkflowCommand.NotifyCanExecuteChanged();
-        PreviewSelectedWorkflowNodeCommand.NotifyCanExecuteChanged();
-        OnPropertyChanged(nameof(WorkflowRunGuardText));
-    }
 }
