ea42145eb5f2a0f9300d84864d5765f7b87a84fd
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 12:30:13 +0800

    拆分数据预览来源文案

--- Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Presentation.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Presentation.cs
@@ -2,13 +2,6 @@ namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    public string DataPreviewSourceText =>
-        !string.IsNullOrWhiteSpace(dataPreviewSourceWorkflowRunId)
-        && !string.IsNullOrWhiteSpace(dataPreviewSourceNodeInstanceId)
-        && !string.IsNullOrWhiteSpace(dataPreviewSourceLogicalTableId)
-            ? FormatDataPreviewSourceText()
-            : T("data_preview.source_not_loaded");
-
     public string DataPreviewSectionText => T("definition.data_preview");
 
     public string DataPreviewEmptyText => T("definition.data_preview_empty");
@@ -20,27 +13,4 @@ public partial class MainWindowViewModel
     public string PreviewSelectedNodeText => T("definition.preview_selected_node");
 
     public string TableRefsSectionText => T("data.table_refs");
-
-    private string FormatDataPreviewSourceText()
-    {
-        if (string.Equals(
-                dataPreviewSourceRunMode,
-                "preview_to_node",
-                System.StringComparison.OrdinalIgnoreCase))
-        {
-            return F(
-                "format.data_preview_source_preview",
-                dataPreviewSourceWorkflowRunId!,
-                string.IsNullOrWhiteSpace(dataPreviewSourceTargetNodeInstanceId)
-                    ? dataPreviewSourceNodeInstanceId!
-                    : dataPreviewSourceTargetNodeInstanceId!,
-                dataPreviewSourceLogicalTableId!);
-        }
-
-        return F(
-            "format.data_preview_source_full",
-            dataPreviewSourceWorkflowRunId!,
-            dataPreviewSourceNodeInstanceId!,
-            dataPreviewSourceLogicalTableId!);
-    }
 }
