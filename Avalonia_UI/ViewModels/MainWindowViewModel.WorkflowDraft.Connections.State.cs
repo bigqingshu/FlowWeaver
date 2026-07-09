90473fdb59dea6aa4543490839399c61bc78fa71
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 03:26:02 +0800

    拆分工作流连接高级区可见状态

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Connections.State.cs
@@ -0,0 +1,9 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    [ObservableProperty]
+    private bool isWorkflowConnectionsAdvancedVisible;
+}
--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Connections.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Connections.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Linq;
-using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    [ObservableProperty]
-    private bool isWorkflowConnectionsAdvancedVisible;
-
     private void ClearSelectedWorkflowDefinitionDraftConnectionIfMissing()
     {
         if (string.IsNullOrWhiteSpace(SelectedWorkflowDefinitionDraftConnectionId))
