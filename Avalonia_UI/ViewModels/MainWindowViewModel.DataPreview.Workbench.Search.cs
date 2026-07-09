8b1d166dcfc6c510722c944f45aae4095bb2acb7
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 00:37:03 +0800

    提取数据预览工作台搜索刷新方法

--- Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Search.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Search.cs
@@ -10,18 +10,26 @@ public partial class MainWindowViewModel
             dataPreviewWorkbenchEditableCellRows,
             DataPreviewWorkbenchSearchText);
 
+        RefreshDataPreviewWorkbenchColumns();
+        RefreshDataPreviewWorkbenchRows(visibleRowIndexes);
+        NotifyDataPreviewWorkbenchRowsChanged();
+    }
+
+    private void RefreshDataPreviewWorkbenchColumns()
+    {
         DataPreviewWorkbenchColumns.Clear();
         foreach (var column in dataPreviewWorkbenchLoadedColumns)
         {
             DataPreviewWorkbenchColumns.Add(new TableDataPreviewColumnViewModel(column));
         }
+    }
 
+    private void RefreshDataPreviewWorkbenchRows(int[] visibleRowIndexes)
+    {
         DataPreviewWorkbenchRows.Clear();
         foreach (var rowIndex in visibleRowIndexes)
         {
             DataPreviewWorkbenchRows.Add(CreateDataPreviewWorkbenchRow(rowIndex));
         }
-
-        NotifyDataPreviewWorkbenchRowsChanged();
     }
 }
