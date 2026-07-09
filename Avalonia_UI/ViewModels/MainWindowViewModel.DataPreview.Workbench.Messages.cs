4bd570b81ad63a530df09fef39668f6014d66ef0
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 01:00:49 +0800

    拆分数据预览工作台加载消息格式化

--- Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Messages.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Messages.cs
@@ -11,15 +11,25 @@ public partial class MainWindowViewModel
 
         var filter = NormalizeFilter(DataPreviewWorkbenchSearchText);
         DataPreviewWorkbenchMessage = filter is null
-            ? F(
-                "format.loaded_data_preview_table_rows",
-                dataPreviewWorkbenchLoadedRows.Length,
-                dataPreviewWorkbenchRowCount,
-                LoadedDataPreviewTableRef.LogicalTableId)
-            : F(
-                "format.data_preview_search_matches",
-                DataPreviewWorkbenchRows.Count,
-                dataPreviewWorkbenchLoadedRows.Length,
-                filter);
+            ? FormatDataPreviewWorkbenchLoadedRowsMessage()
+            : FormatDataPreviewWorkbenchSearchMatchesMessage(filter);
+    }
+
+    private string FormatDataPreviewWorkbenchLoadedRowsMessage()
+    {
+        return F(
+            "format.loaded_data_preview_table_rows",
+            dataPreviewWorkbenchLoadedRows.Length,
+            dataPreviewWorkbenchRowCount,
+            LoadedDataPreviewTableRef!.LogicalTableId);
+    }
+
+    private string FormatDataPreviewWorkbenchSearchMatchesMessage(string filter)
+    {
+        return F(
+            "format.data_preview_search_matches",
+            DataPreviewWorkbenchRows.Count,
+            dataPreviewWorkbenchLoadedRows.Length,
+            filter);
     }
 }
