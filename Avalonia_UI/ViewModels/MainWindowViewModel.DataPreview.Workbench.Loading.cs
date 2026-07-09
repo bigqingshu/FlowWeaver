b980fc18d2b355f652d961f0fbd053b2d22ea7b8
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 00:22:05 +0800

    拆分数据预览工作台表格加载实现

--- Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Loading.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Loading.cs
@@ -28,57 +28,6 @@ public partial class MainWindowViewModel
             dataPreviewWorkbenchOffset + DataPreviewRowLimit);
     }
 
-    private async Task LoadDataPreviewWorkbenchTablePageAsync(
-        TableRefListItemViewModel? requestedTableRef,
-        int offset)
-    {
-        if (requestedTableRef is null)
-        {
-            return;
-        }
-
-        var requestedTableRefId = requestedTableRef.TableRefId;
-        var requestVersion = ++dataPreviewWorkbenchLoadVersion;
-        IsLoadingDataPreviewWorkbench = true;
-        DataPreviewWorkbenchMessage = F(
-            "format.loading_data_preview_table",
-            requestedTableRef.LogicalTableId);
-        DataPreviewWorkbenchErrorMessage = null;
-
-        try
-        {
-            var response = await _apiClient.GetTableDataRowsAsync(
-                BuildSettings(),
-                requestedTableRefId,
-                offset: Math.Max(0, offset),
-                limit: DataPreviewRowLimit,
-                cancellationToken: _shutdown.Token);
-
-            if (IsStaleDataPreviewWorkbenchRequest(requestVersion))
-            {
-                return;
-            }
-
-            if (!response.Ok || response.Data is null)
-            {
-                DataPreviewWorkbenchMessage = T("data_preview.workbench_load_failed");
-                DataPreviewWorkbenchErrorMessage = DescribeError(response);
-                return;
-            }
-
-            LoadDataPreviewWorkbenchRows(response.Data);
-            LoadedDataPreviewTableRef = requestedTableRef;
-            UpdateDataPreviewWorkbenchLoadedMessage();
-        }
-        finally
-        {
-            if (requestVersion == dataPreviewWorkbenchLoadVersion)
-            {
-                IsLoadingDataPreviewWorkbench = false;
-            }
-        }
-    }
-
     private bool IsStaleDataPreviewWorkbenchRequest(int requestVersion)
     {
         return requestVersion != dataPreviewWorkbenchLoadVersion;
