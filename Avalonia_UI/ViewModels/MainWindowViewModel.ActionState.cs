1f68544393ab6119fdb1e920a8ecc5ba9f068bc2
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 22:02:34 +0800

    拆分全局动作状态刷新入口

--- Avalonia_UI/ViewModels/MainWindowViewModel.ActionState.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.ActionState.cs
@@ -5,9 +5,30 @@ public partial class MainWindowViewModel
     private void NotifyEngineActionStateChanged()
     {
         OnPropertyChanged(nameof(CanUseEngineActions));
+        NotifyRunSelectionActionStateChanged();
+        NotifyNodeCatalogSummaryActionStateChanged();
+        NotifyWorkflowListActionStateChanged();
+        NotifyWorkflowExecutionActionStateChanged();
+        NotifyRunMonitorActionStateChanged();
+        NotifyWorkflowDraftActionStateChanged();
+        NotifyRuntimeEventLogActionStateChanged();
+        NotifyDataPreviewActionStateChanged();
+        NotifySharedPublicationActionStateChanged();
+    }
+
+    private void NotifyRunSelectionActionStateChanged()
+    {
         OnPropertyChanged(nameof(CanUseCancelSelectedRunAction));
         OnPropertyChanged(nameof(CancelSelectedRunDisabledReasonText));
+    }
+
+    private void NotifyNodeCatalogSummaryActionStateChanged()
+    {
         OnPropertyChanged(nameof(RefreshNodeDefinitionsDisabledReasonText));
+    }
+
+    private void NotifyWorkflowListActionStateChanged()
+    {
         RefreshWorkflowsCommand.NotifyCanExecuteChanged();
         CreateTemplateWorkflowCommand.NotifyCanExecuteChanged();
         ImportWorkflowCommand.NotifyCanExecuteChanged();
@@ -19,28 +40,57 @@ public partial class MainWindowViewModel
         OnPropertyChanged(nameof(ExportSelectedWorkflowDisabledReasonText));
         OnPropertyChanged(nameof(CanUseDeleteSelectedWorkflowAction));
         OnPropertyChanged(nameof(DeleteSelectedWorkflowDisabledReasonText));
+    }
+
+    private void NotifyWorkflowExecutionActionStateChanged()
+    {
         StartSelectedWorkflowCommand.NotifyCanExecuteChanged();
         PreviewSelectedWorkflowNodeCommand.NotifyCanExecuteChanged();
+    }
+
+    private void NotifyRunMonitorActionStateChanged()
+    {
         RefreshRunsCommand.NotifyCanExecuteChanged();
         CancelSelectedRunCommand.NotifyCanExecuteChanged();
         RefreshNodeRunsCommand.NotifyCanExecuteChanged();
+    }
+
+    private void NotifyWorkflowDraftActionStateChanged()
+    {
         LoadSelectedWorkflowDefinitionCommand.NotifyCanExecuteChanged();
         RefreshNodeDefinitionsCommand.NotifyCanExecuteChanged();
         ValidateWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
         ApplySelectedNodeConfigDraftCommand.NotifyCanExecuteChanged();
-        ApplyRuntimeOptionsDraftCommand.NotifyCanExecuteChanged();
-        RegenerateRuntimeOptionsJsonDraftCommand.NotifyCanExecuteChanged();
-        ResetRuntimeOptionsSelectedNodeOverrideCommand.NotifyCanExecuteChanged();
+        NotifyRuntimeOptionsActionStateChanged();
         AddWorkflowDefinitionDraftNodeCommand.NotifyCanExecuteChanged();
         NotifyWorkflowDefinitionNodeActionCommandsChanged();
         AddWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
         DeleteWorkflowDefinitionDraftConnectionCommand.NotifyCanExecuteChanged();
         SaveWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
+    }
+
+    private void NotifyRuntimeOptionsActionStateChanged()
+    {
+        ApplyRuntimeOptionsDraftCommand.NotifyCanExecuteChanged();
+        RegenerateRuntimeOptionsJsonDraftCommand.NotifyCanExecuteChanged();
+        ResetRuntimeOptionsSelectedNodeOverrideCommand.NotifyCanExecuteChanged();
+    }
+
+    private void NotifyRuntimeEventLogActionStateChanged()
+    {
         RefreshRuntimeEventLogCommand.NotifyCanExecuteChanged();
+    }
+
+    private void NotifyDataPreviewActionStateChanged()
+    {
         RefreshTableRefsCommand.NotifyCanExecuteChanged();
         RefreshSelectedWorkflowNodeDataPreviewCommand.NotifyCanExecuteChanged();
         ShowDataPreviewDetailsCommand.NotifyCanExecuteChanged();
         LoadSelectedDataPreviewTableCommand.NotifyCanExecuteChanged();
+    }
+
+    private void NotifySharedPublicationActionStateChanged()
+    {
         RefreshSharedPublicationsCommand.NotifyCanExecuteChanged();
         RefreshSharedPublicationVersionsCommand.NotifyCanExecuteChanged();
     }
