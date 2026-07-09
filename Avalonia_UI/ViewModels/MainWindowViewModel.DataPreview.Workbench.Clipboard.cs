5b0659c7b94ae34e995ff81f8296a6b1ac3050dc
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 00:41:01 +0800

    拆分数据预览工作台TSV构建辅助

--- Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Clipboard.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.Workbench.Clipboard.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using Avalonia_UI.Models;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Avalonia_UI.ViewModels;
@@ -10,13 +8,5 @@ public partial class MainWindowViewModel
     private void CopyDataPreviewWorkbenchTsv()
     {
         DataPreviewWorkbenchClipboardText = BuildDataPreviewWorkbenchTsv();
-    }
-
-    private string BuildDataPreviewWorkbenchTsv()
-    {
-        return DataPreviewTableGridBuilder.BuildTsv(
-            DataPreviewWorkbenchColumns.Select(column => column.Name),
-            DataPreviewWorkbenchRows.Select(
-                row => row.Cells.Select(cell => cell.Text)));
     }
 }
