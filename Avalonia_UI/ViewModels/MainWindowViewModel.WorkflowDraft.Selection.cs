049b6ccc69afe3eef88a3549dc41a84c845d37a7
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 14:35:35 +0800

    拆分工作流节点选择变更处理

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Selection.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Selection.cs
@@ -11,20 +11,4 @@ public partial class MainWindowViewModel
     public bool HasSelectedWorkflowDefinitionNode => SelectedWorkflowDefinitionNode is not null;
 
     public bool HasNoSelectedWorkflowDefinitionNode => SelectedWorkflowDefinitionNode is null;
-
-    partial void OnSelectedWorkflowDefinitionNodeChanged(
-        WorkflowDefinitionNodeListItemViewModel? value)
-    {
-        OnPropertyChanged(nameof(HasSelectedWorkflowDefinitionNode));
-        OnPropertyChanged(nameof(HasNoSelectedWorkflowDefinitionNode));
-        ResetDataPreviewSelectionState();
-        RefreshSelectedNodeDisplayNameDraftState();
-        RefreshSelectedNodeConfigDraftState();
-        SelectedRuntimeOptionsNode = value;
-        ApplySelectedNodeDisplayNameDraftCommand.NotifyCanExecuteChanged();
-        ApplySelectedNodeConfigDraftCommand.NotifyCanExecuteChanged();
-        NotifyWorkflowDefinitionNodeActionCommandsChanged();
-        RefreshSelectedWorkflowNodeDataPreviewCommand.NotifyCanExecuteChanged();
-        PreviewSelectedWorkflowNodeCommand.NotifyCanExecuteChanged();
-    }
 }
