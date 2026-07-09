3fad4d021ad5b2217ad1b2d20637e95b868aa4e9
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-10 03:44:10 +0800

    拆分节点目录刷新启用条件

--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Catalog.Commands.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.Catalog.Commands.cs
@@ -5,11 +5,6 @@ namespace Avalonia_UI.ViewModels;
 
 public partial class MainWindowViewModel
 {
-    private bool CanRefreshNodeDefinitions()
-    {
-        return CanUseEngineActions && !IsLoadingNodeDefinitions;
-    }
-
     [RelayCommand(CanExecute = nameof(CanRefreshNodeDefinitions))]
     private async Task RefreshNodeDefinitionsAsync()
     {
