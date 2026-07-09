1a1b063bd13c58e7bdcab1565ff2f8c9f85e85cb
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 17:36:53 +0800

    拆分数据预览节点预览加载变更处理

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.DataPreview.NodePreview.Loading.ChangeHandlers.cs
@@ -0,0 +1,11 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    partial void OnIsLoadingDataPreviewChanged(bool value)
+    {
+        OnPropertyChanged(nameof(IsDataPreviewBusy));
+        RefreshSelectedWorkflowNodeDataPreviewCommand.NotifyCanExecuteChanged();
+        PreviewSelectedWorkflowNodeCommand.NotifyCanExecuteChanged();
+    }
+}
