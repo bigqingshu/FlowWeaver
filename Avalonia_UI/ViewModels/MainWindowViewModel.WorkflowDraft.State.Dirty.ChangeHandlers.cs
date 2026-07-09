619ec4fb6d05cd683447acb0e73f15b51af0a9de
bigqingshu <104840173+bigqingshu@users.noreply.github.com>
2026-07-09 15:14:12 +0800

    拆分工作流草稿脏状态变更处理

---
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Dirty.ChangeHandlers.cs
@@ -0,0 +1,13 @@
+namespace Avalonia_UI.ViewModels;
+
+public partial class MainWindowViewModel
+{
+    partial void OnIsWorkflowDefinitionDraftDirtyChanged(bool value)
+    {
+        RestoreWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
+        SaveWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
+        StartSelectedWorkflowCommand.NotifyCanExecuteChanged();
+        PreviewSelectedWorkflowNodeCommand.NotifyCanExecuteChanged();
+        OnPropertyChanged(nameof(WorkflowRunGuardText));
+    }
+}
--- Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Dirty.cs
+++ Avalonia_UI/ViewModels/MainWindowViewModel.WorkflowDraft.State.Dirty.cs
@@ -6,13 +6,4 @@ public partial class MainWindowViewModel
 {
     [ObservableProperty]
     private bool isWorkflowDefinitionDraftDirty;
-
-    partial void OnIsWorkflowDefinitionDraftDirtyChanged(bool value)
-    {
-        RestoreWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
-        SaveWorkflowDefinitionDraftCommand.NotifyCanExecuteChanged();
-        StartSelectedWorkflowCommand.NotifyCanExecuteChanged();
-        PreviewSelectedWorkflowNodeCommand.NotifyCanExecuteChanged();
-        OnPropertyChanged(nameof(WorkflowRunGuardText));
-    }
 }
